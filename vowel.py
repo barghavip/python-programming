f52cd7605f29326bc8e91f6c6dced79de827196d
barghvi P <41780765+barghavip@users.noreply.github.com>
2019-08-08 19:02:00 +0530

    Add files via upload

---
+++ vowel.py
@@ -0,0 +1,9 @@
+a=input()
+v=['a','e','i','o','u']
+c=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','u','v','w','x','y','z']
+if a in v:
+    print('Vowel')
+elif a in c:
+    print('consonant')
+else:
+    print('invalid')
