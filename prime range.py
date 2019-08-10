693612dc108e8a9e8a6e512b57f6c32ac4ae9afe
barghvi P <41780765+barghavip@users.noreply.github.com>
2019-08-10 20:07:13 +0530

    Add files via upload

---
+++ prime range.py
@@ -0,0 +1,11 @@
+start=int(input())
+end=int(input())
+for x in range(start,end+1):
+    if x>1:
+        for i in range(2,x):
+            if(x%i)==0:
+             break
+        else:
+            print(x)
+        
+    
