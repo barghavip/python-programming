af41f24235952a5190a429cec07fb30ecdc33e18
barghvi P <41780765+barghavip@users.noreply.github.com>
2019-08-10 20:01:38 +0530

    Add files via upload

--- ams.py
+++ ams.py
@@ -1,14 +1,12 @@
-num = int(input("enter a number: "))
- 
-length = len(str(num))
-sum = 0
-temp = num
- 
-while(temp != 0):
-	sum = sum + ((temp % 10) ** length)
-	temp = temp // 10
- 
-if sum == num:
-	print("armstrong number")
-else:
-	print("not armstrong number")
+n1=int(input())
+n2=int(input())
+for i in range(n1,n2+1): 
+    l=len(str(i))
+    t=i
+    sum=0
+    while t>0:
+        sum=sum+(t%10)**l
+        t=t//10
+    if sum==i:
+        print(i)
+    
